c628f04d266100e08880b8f686cdcad43abecbcb
YOUSSEFLJ9 <youssefmomen@outlook.com>
2025-09-13 20:46:53 +0100

    check1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1= 10
+number2 = 5
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print("Addition of", number1, "and", number2, "is:", sum_result)
+print("Subtraction of", number2, "from", number1, "is:", difference_result)
+print("Multiplication of", number1, "and", number2, "is:", product_result)
