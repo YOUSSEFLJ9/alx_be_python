bbe4c9aa56c02d8e386189be0d28b2b24e9346a9
YOUSSEFLJ9 <youssefmomen@outlook.com>
2025-09-26 21:15:16 +0100

    ex02

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,18 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    # print(f"Current Date and Time: {formatted_date}")
+    return formatted_date
+
+def calculate_future_date(days_ahead):
+    future_date = datetime.datetime.now() + datetime.timedelta(days_ahead)
+    formatted_future_date = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    # print(f"Date after {days_ahead} days: {formatted_future_date}")
+    return formatted_future_date
+
+print(f"Current date and time: {display_current_datetime()}")
+
+input_days = int(input("Enter number of days to calculate future date: "))
+print(f"Future date: {calculate_future_date(input_days)}")
